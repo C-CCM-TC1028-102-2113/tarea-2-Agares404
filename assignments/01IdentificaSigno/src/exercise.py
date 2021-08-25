aa7363f6b8c81c3b63b3fc85cc7c4f63f92a478c
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-25 17:19:44 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,6 +1,14 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    print("Identificar el signo")
+    num=int(input("Dame un número: "))
+    if num == 0:
+        print("El número ingresado es cero")
+    elif num < 0:
+        print("El número ingresado es Negativo")
+    else:
+        print("El número ingresado es Positivo")
     pass
     
 
