76ea82d5fd4f65e96f6c75923b56267d58e41f54
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-25 18:26:38 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("El mayor de tres números")
+    a=int(input("Introduzca el primer número: "))
+    b=int(input("Introduzca el segundo número: "))
+    c=int(input("Introduzca el tercer número: "))
+
+    if a > b and a > c:
+        print(a)
+    elif b > a and b > c:
+        print(b)
+    elif  c > a and c > b:
+        print(c)
+    else:
+        print("Alguno de los valores son iguales")
     pass
 
 
