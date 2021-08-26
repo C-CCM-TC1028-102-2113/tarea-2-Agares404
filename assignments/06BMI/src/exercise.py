c5934994116c2da60de7470b5c606fd7805c4998
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-25 19:09:20 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Ordena 3 numeros")
+    p = float(input("Ingresa tu peso en kg: "))
+    a = float(input("Ingresa tu altura en m: "))
+
+    imc=p/(a*a)
+
+    if imc < 20:
+        print("PESO BAJO")
+    elif imc >= 20 and imc < 25:
+        print("NORMAL")
+    elif imc >= 25 and imc < 30:
+        print("SOBREPESO")
+    elif imc >= 30 and imc < 40:
+        print("OBESIDAD")
+    elif imc >= 40:
+        print("OBESIDAD MORBIDA")
+    else:
+        print("Revisa tus datos, alguno de ellos es erróneo")
     pass
 if __name__=='__main__':
     main()
